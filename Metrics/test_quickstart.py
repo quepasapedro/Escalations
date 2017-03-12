464bb9a01341721b89377b0f37e1f74d9d6cd049
Peter King <peter@petertking.net>
2017-03-12 13:26:28 -0700

    add updated Google Sheets API test script.

---
+++ Metrics/test_quickstart.py
@@ -0,0 +1,120 @@
+from __future__ import print_function
+import httplib2
+import os
+import json
+
+from apiclient import discovery
+from oauth2client import client
+from oauth2client import tools
+from oauth2client.file import Storage
+
+try:
+    import argparse
+    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+except ImportError:
+    flags = None
+
+# If modifying these scopes, delete your previously saved credentials
+# at ~/.credentials/sheets.googleapis.com-python-quickstart.json
+SCOPES = 'https://www.googleapis.com/auth/spreadsheets.readonly'
+CLIENT_SECRET_FILE = 'client_secret.json'
+APPLICATION_NAME = 'Google Sheets API Python Quickstart'
+
+
+def get_credentials():
+    """
+    Gets valid user credentials from storage.
+
+    If nothing has been stored, or if the stored credentials are invalid,
+    the OAuth2 flow is completed to obtain the new credentials.
+
+    Returns:
+        Credentials, the obtained credential.
+    """
+    home_dir = os.path.expanduser('~')
+    credential_dir = os.path.join(home_dir, '.credentials')
+    if not os.path.exists(credential_dir):
+        os.makedirs(credential_dir)
+    credential_path = os.path.join(credential_dir,
+                                   'sheets.googleapis.com-python-quickstart.json')
+
+    store = Storage(credential_path)
+    credentials = store.get() # retrieves credentials from the "store" instance of the "Storage" class pointing at cred_dir
+
+    # if not credentials or credentials.invalid:
+    #     flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+    #     flow.user_agent = APPLICATION_NAME
+    #     if flags:
+    #         credentials = tools.run_flow(flow, store, flags)
+    #     else: # Needed only for compatibility with Python 2.6
+    #         credentials = tools.run(flow, store)
+    #     print('Storing credentials to ' + credential_path)
+
+    return credentials
+
+def main():
+    """Shows basic usage of the Sheets API.
+
+    Creates a Sheets API service object and prints the names and majors of
+    students in a sample spreadsheet:
+    https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+    """
+    credentials = get_credentials()
+    http = credentials.authorize(httplib2.Http())
+    discoveryUrl = ('https://sheets.googleapis.com/$discovery/rest?'
+                    'version=v4')
+    service = discovery.build('sheets', 'v4', http=http,
+                              discoveryServiceUrl=discoveryUrl)
+
+    spreadsheetId = '1yphtzhkhamAe62O4dgsfecBbVJ3DTnYeBT_LPHj7ZVs'
+    rangeName = 'Sheet1!B:Q'
+    result = service.spreadsheets().values().get(
+        spreadsheetId=spreadsheetId, range=rangeName).execute()
+    values = result.get('values', {})
+
+    # values_json = json.dumps(values)
+
+
+
+
+    if not values:
+        print('No data found.')
+    else:
+        return values
+        # for row in values:
+        #     # Print columns A and E, which correspond to indices 0 and 4.
+        #     print('%s, %s' % (row[0], row[4]))
+
+
+if __name__ == '__main__':
+
+    def create_dict(values_list):
+
+        utf_values = []
+        for list in values_list:
+            temp_list = []
+            for item in list:
+                temp_list.append(item.encode('utf-8'))
+            utf_values.append(temp_list)
+
+        key_list = []
+        for item in utf_values[0]:
+            key_list.append(item)
+
+        del(utf_values[0])
+
+        parsed_dict = []
+        for list in utf_values:
+            key_index = 0
+            row_dict = {}
+            for item in list:
+                row_dict[key_list[key_index]] = item
+                key_index += 1
+            parsed_dict.append(row_dict)
+
+        print(parsed_dict)
+
+
+
+    values_list = main()
+    create_dict(values_list)
